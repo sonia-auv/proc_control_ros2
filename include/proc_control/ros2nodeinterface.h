20e5a6b70369230bc4f577e90a8415d278f34e02
Nimai Jariwala <nimai.jariwala.1@ens.etsmtl.ca>
2025-12-05 06:47:25 -0500

    Lower Vel + eq orien

--- include/proc_control/ros2nodeinterface.h
+++ include/proc_control/ros2nodeinterface.h
@@ -6,7 +6,7 @@
 //
 // Model version                  : 2.47
 // Simulink Coder version         : 24.2 (R2024b) 21-Jun-2024
-// C/C++ source code generated on : Fri Dec 05 04:00:12 2025
+// C/C++ source code generated on : Fri Dec 05 06:42:07 2025
 //
 #ifndef _ROS2_MATLAB_NODEINTERFACE_
 #define _ROS2_MATLAB_NODEINTERFACE_
--- include/proc_control/slros2_initialize.h
+++ include/proc_control/slros2_initialize.h
@@ -1,5 +1,5 @@
 // Copyright 2022-2024 The MathWorks, Inc.
-// Generated 05-Dec-2025 04:00:10
+// Generated 05-Dec-2025 06:42:05
 #ifndef _SLROS2_INITIALIZE_H_
 #define _SLROS2_INITIALIZE_H_
 #include "proc_control_types.h"
